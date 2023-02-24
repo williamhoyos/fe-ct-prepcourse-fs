3e242040388990391057e69aa361aff5f0557e20
williamhoyos <williamja.hoyos@gmail.com>
2023-02-24 14:42:14 -0500

    m03

---
+++ M03 JavaScript Fundamentos/test/prueba.js
@@ -0,0 +1,24 @@
+
+function suma(x){
+    console.log(x + 3);
+}
+
+function suma(x){
+    return x + 3;
+}
+
+var suma = function (X){
+    return x + 3;
+};
+
+
+function viajar(destino){
+    if (destino === 'brasil'){
+        console.log('gire a la izq');
+    } else if (destino === 'argentina'){
+        console.log('gire a la der');
+    } else {
+        console.log('nos perdimos');
+    }
+}
+viajar ('argentina');
